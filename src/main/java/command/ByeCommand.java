ba0ce6eb631c4ab79f384fa0ba0e9acd85ce79a2
junlee1991 <julio_harjo@hotmail.sg>
2022-09-22 20:07:33 +0800

    Remove unnecessary comments and code

--- src/main/java/command/ByeCommand.java
+++ src/main/java/command/ByeCommand.java
@@ -3,13 +3,17 @@ package command;
 import exceptions.DukeException;
 import duke.Storage;
 import duke.Ui;
+import javafx.application.Platform;
 import tasks.TaskList;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 /***
  * This class has to do with exiting the program
  */
 
-public class ByeCommand extends Command{
+public class ByeCommand extends Command {
 
     /**
      * constructor for ByeCommand
@@ -25,8 +29,20 @@ public class ByeCommand extends Command{
      * @param storage
      * @throws DukeException
      */
-    public String execute(TaskList taskList, Ui ui, Storage storage) throws DukeException{
-            setExitTrue();
-            return ui.exit();
+    public String execute(TaskList taskList, Ui ui, Storage storage) throws DukeException {
+            try{
+                setExitTrue();
+                return ui.exit();
+            }finally{
+                // taken from github
+                //https://github.com/nus-cs2103-AY2223S1/forum/issues/287
+                new Timer().schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        Platform.exit();
+                        System.exit(0);
+                    }
+                }, 2000);
+            }
     }
 }
--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -2,16 +2,9 @@ package duke;
 
 import java.io.File;
 import java.io.IOException;
-
 import tasks.TaskList;
 import command.Command;
 import exceptions.*;
-
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
 
 import javafx.scene.image.Image;
 
@@ -27,34 +20,13 @@ public class Duke {
     private TaskList tasks;
     private Ui ui;
 
-    // Tutorial part 2 -----------------------
-    private ScrollPane scrollPane;
-    private VBox dialogContainer;
-    private TextField userInput;
-    private Button sendButton;
-    private Scene scene;
-
     private Image user = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
     private Image duke = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
 
     /***
-     * Constructor for Duke.
-     * @param directory
-     * @param filePath
-     *once duke gets initiated, create duke.Ui object, create storage
+     * Constructor for Duke
+     * Creates new storage and UI object once initialized
      */
-    public Duke(String directory, String filePath) {
-        ui = new Ui();
-        storage = new Storage(directory, filePath);
-        try {
-            tasks = new TaskList(storage.load());
-        } catch (IOException e) {
-            // print some error
-            ui.showLoadingError();
-            tasks = new TaskList();
-        }
-    }
-
     public Duke() {
         ui = new Ui();
         storage = new Storage(DIRECTORY_NAME, FILE_NAME);
@@ -88,12 +60,6 @@ public class Duke {
      * A method to runs the program.
      */
     public void run() {
-//        try {
-//            Command stuff = new GreetCommand(tasks, ui, storage);
-//            stuff.execute(tasks, ui, storage);
-//        }catch (DukeException e) {
-//            ui.showError(e.getMessage());
-//        }
         boolean isExit = false;
         while (!isExit) {
             try {
@@ -107,132 +73,10 @@ public class Duke {
                 ui.showError(e.getMessage());
             }
         }
+
     }
 
     public static void main(String[] args) {
         new Duke().run();
-//        new Duke(DIRECTORY_NAME, FILE_NAME).run();
-
     }
 }
-
-//    @Override
-//    public void start(Stage stage) throws Exception {
-//        // Tutorial Part 1
-//        //step 1: set up the required component
-//        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
-//        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
-//
-//        stage.setScene(scene); // Setting the stage to show our screen
-//        stage.show(); // Render the stage.
-////------------------------------------------------------------------------
-//        //Part ii--------------------------------
-//        //The container for the content of the chat to scroll.
-//        scrollPane = new ScrollPane();
-//        dialogContainer = new VBox();
-//        scrollPane.setContent(dialogContainer);
-//
-//        userInput = new TextField();
-//        sendButton = new Button("Send");
-//
-//        AnchorPane mainLayout = new AnchorPane();
-//        mainLayout.getChildren().addAll(scrollPane, userInput,  sendButton);
-//    // -------------------------------------------------------------------
-//        //Step 2. Formatting the window to look as expected
-//        stage.setTitle("Duke");
-//        stage.setResizable(false);
-//        stage.setMinHeight(600.0);
-//        stage.setMinWidth(400.0);
-//
-//        mainLayout.setPrefSize(400.0, 600.0);
-//
-//        scrollPane.setPrefSize(385, 535);
-//        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-//        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-//
-//        scrollPane.setVvalue(1.0);
-//        scrollPane.setFitToWidth(true);
-//
-//        // You will need to import `javafx.scene.layout.Region` for this.
-//        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-//
-//        userInput.setPrefWidth(325.0);
-//
-//        sendButton.setPrefWidth(55.0);
-//
-//        AnchorPane.setTopAnchor(scrollPane, 1.0);
-//
-//        AnchorPane.setBottomAnchor(sendButton, 1.0);
-//        AnchorPane.setRightAnchor(sendButton, 1.0);
-//
-//        AnchorPane.setLeftAnchor(userInput , 1.0);
-//        AnchorPane.setBottomAnchor(userInput, 1.0);
-//
-//        scene = new Scene(mainLayout);
-//
-//        stage.setScene(scene);
-//        stage.show();
-//
-//        // step 3 Iteration 1:
-//        sendButton.setOnMouseClicked((event) -> {
-//            dialogContainer.getChildren().add(getDialogLabel(userInput.getText()));
-//            userInput.clear();
-//        });
-//
-//        userInput.setOnAction((event) -> {
-//            dialogContainer.getChildren().add(getDialogLabel(userInput.getText()));
-//            userInput.clear();
-//        });
-//
-//        // remedy
-//        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
-//
-//        //Part 3. Add functionality to handle user input.
-//        sendButton.setOnMouseClicked((event) -> {
-//            handleUserInput();
-//        });
-//
-//        userInput.setOnAction((event) -> {
-//            handleUserInput();
-//        });
-//
-//    }
-//
-//    private Label getDialogLabel(String text) {
-//        // You will need to import `javafx.scene.control.Label`.
-//        Label textToAdd = new Label(text);
-//        textToAdd.setWrapText(true);
-//
-//        return textToAdd;
-//    }
-
-//
-//    /**
-//     * Iteration 2:
-//     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
-//     * the dialog container. Clears the user input after processing.
-//     */
-//    private void handleUserInput() {
-//        Label userText = new Label(userInput.getText());
-//        Label dukeText = new Label(getResponse(userInput.getText()));
-//        dialogContainer.getChildren().addAll(
-//                new DialogBox(userText, new ImageView(user)),
-//                new DialogBox(dukeText, new ImageView(duke))
-//        );
-//        userInput.clear();
-//    }
-
-
-//    private void handleUserInput() {
-//        Label userText = new Label(userInput.getText());
-//        Label dukeText = new Label(getResponse(userInput.getText()));
-//        dialogContainer.getChildren().addAll(
-//                DialogBox.getUserDialog(userText, new ImageView(user)),
-//                DialogBox.getDukeDialog(dukeText, new ImageView(duke))
-//        );
-//        userInput.clear();
-//    }
-
-
-
-
